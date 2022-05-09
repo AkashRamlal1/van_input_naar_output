52b74009c23f72eed2b89835d54d2eaf6fa44887
AkashRamlal1 <90188404+AkashRamlal1@users.noreply.github.com>
2022-05-09 10:18:38 +0200

    Add files via upload

---
+++ person.py
@@ -0,0 +1,16 @@
+from tkinter import N
+
+
+naam = input('wat is je naam ')
+adress = input('wat is je adress ')
+postcode = input('wat is je postcode ')
+woonplaats = input('wat is je woonplaats ')
+
+print("--------------------------------------------")
+print(f'adresskaart {naam}')
+print('\n\n')
+print(f'| naam: {naam} ')
+print(f'| adress: {adress}')
+print(f'| postcode: {postcode}')
+print(f'| woonplaats: {woonplaats}')
+print("--------------------------------------------")
